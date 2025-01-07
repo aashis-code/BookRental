47dc7af11174a5c69563d15016e440475fb2b362
Aashis Pandey <aashispandey2080@gmai.com>
2025-01-07 21:22:52 +0545

    fix: gitignore file changes.

--- src/main/java/com/bookrental/repository/BookTransactionRepo.java
+++ src/main/java/com/bookrental/repository/BookTransactionRepo.java
@@ -22,26 +22,11 @@ public interface BookTransactionRepo extends JpaRepository<BookTransaction, Inte
 	@Query(value = "select bt.id,bt.rent_status,bt.created_by ,bt.from_date , bt.to_date ,(bt.to_date::DATE-CURRENT_DATE::DATE) as days_left,\r\n"
 			+ "to_char(bt.created_date, 'YYYY-MM-DD HH24:MI:SS') as created_date, bt.last_modified_by, \r\n"
 			+ "to_char(bt.modified_date, 'YYYY-MM-DD HH24:MI:SS') as last_modified_date, m.name as member_name, \r\n"
-			+ "b.name as book_name from book_transaction bt inner join member m on bt.member_id=m.id inner join book b on bt.book_id=b.id"
+			+ "b.name as book_name from book_transaction bt inner join member m on bt.member_id=m.id inner join book b on bt.book_id=b.id \n"
 			+ "where bt.created_date between coalesce (?1 , bt.created_date) and "
-			+ "coalesce (?2, bt.created_date) and (?3 is null or bt.deleted =?3) and"
+			+ "coalesce (?2, bt.created_date) and (?3 is null or bt.deleted =?3) and "
 			+ "(?4 is null or bt.book_id = ?4) and  (?5 is null or bt.member_id=?5) "
-			+ "and (?6 is null or bt.rent_status=?6)",
-			countQuery = " SELECT \n" +
-					"        COUNT(*)\n" +
-					"    FROM \n" +
-					"        book_transaction bt\n" +
-					"    INNER JOIN \n" +
-					"        member m ON bt.member_id = m.id\n" +
-					"    INNER JOIN \n" +
-					"        book b ON bt.book_id = b.id\n" +
-					"    WHERE \n" +
-					"        bt.created_date BETWEEN COALESCE(?1, bt.created_date) AND COALESCE(?2, bt.created_date)\n" +
-					"        AND (?3 IS NULL OR bt.deleted = ?3)\n" +
-					"        AND (?4 IS NULL OR bt.book_id = ?4)\n" +
-					"        AND (?5 IS NULL OR bt.member_id = ?5)\n" +
-					"        AND (?6 IS NULL OR bt.rent_status = ?6)",
-			nativeQuery = true)
+			+ "and (?6 is null or bt.rent_status=?6) ",	nativeQuery = true)
 	Page<Map<String, Object>> filterBookTransactionAndPagination(	
 			                   LocalDate startDate,
 			                   LocalDate endDate,
