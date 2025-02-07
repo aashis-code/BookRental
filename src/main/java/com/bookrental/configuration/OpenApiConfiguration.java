7ac9de88d539e39fb9da0fedee32d0034fe6024f
Aashis Pandey <aashispandey2080@gmai.com>
2025-02-07 17:48:02 +0545

    fix:token send option set in swager ui

--- src/main/java/com/bookrental/configuration/OpenApiConfiguration.java
+++ src/main/java/com/bookrental/configuration/OpenApiConfiguration.java
@@ -1,8 +1,11 @@
 package com.bookrental.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,11 +22,19 @@ public class OpenApiConfiguration {
                                 .name("Aashis Pandey")
                                 .email("aashis@gmail.com")
                                 .url("")))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth")) // Security for all endpoints
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("BearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
 //                .servers(List.of(
 //                        new Server().url("https://api.example.com/v1").description("Production Server"),
 //                        new Server().url("https://api-staging.example.com/v1").description("Staging Server")
 //                ))
-                ;
+//                ;
     }
 
 
